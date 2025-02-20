aefa837095c35547ca38b78747f1d5d8373f3f5b
switz002 <switz002@rangers.uwp.edu>
2025-02-20 09:21:30 -0600

    Started computing with formulas.

---
+++ python/computing-with-formulas/compare_life_expectancy_to_seconds.py
@@ -0,0 +1,4 @@
+one_billion_seconds_in_years = 10**9*1/60*1/60*1/24*1/365
+print(one_billion_seconds_in_years)
+CDC_value_for_life_expectancy = 77.5
+print(CDC_value_for_life_expectancy > one_billion_seconds_in_years)
---
+++ python/computing-with-formulas/convert_length.py
@@ -0,0 +1,19 @@
+Distance_to_Ashton_From_Parkside_in_km = 5.95 #kg
+Distance_to_Ashton_From_Parkside_in_inches = (Distance_to_Ashton_From_Parkside_in_km*1000*100*1/2.54) #inch
+print(Distance_to_Ashton_From_Parkside_in_inches)
+Distance_to_Ashton_From_Parkside_in_feet = (Distance_to_Ashton_From_Parkside_in_inches*1/12) #ft
+print(Distance_to_Ashton_From_Parkside_in_feet)
+Distance_to_Ashton_From_Parkside_in_yards = (Distance_to_Ashton_From_Parkside_in_feet/3) #yd
+print(Distance_to_Ashton_From_Parkside_in_yards)
+Distance_to_Ashton_From_Parkside_in_miles = (Distance_to_Ashton_From_Parkside_in_yards/1760) #miles
+print(Distance_to_Ashton_From_Parkside_in_miles)
+# Verfication
+Length_in_km = 0.640 # =Km
+Length_in_inches = (Length_in_km*1000*100*1/2.54) #inch)
+print(Length_in_inches)
+Length_in_ft = (Length_in_inches/12)
+print(Length_in_ft)
+Length_in_yards = (Length_in_ft/3)
+print(Length_in_yards)
+Length_in_miles = (Length_in_yards/1760)
+print(Length_in_miles)
