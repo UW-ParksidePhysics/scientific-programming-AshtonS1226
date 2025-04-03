e400410376886b357b202d59e9998aa3944f572b
ashtons1226 <switz002@rangers.uwp.edu>
2025-04-03 09:10:57 -0500

    Entire Command Line 6.

--- python/vpython/ball.py
+++ python/vpython/ball.py
@@ -14,8 +14,13 @@ ball_two = vp.sphere(radius=ball_radius,
 ball_two_velocity = vp.vector(.01, .01, 0)
 ball_two_initial_position = ball_two.pos
 
+times = np.linspace(0,1,100)
+time_interval = np.diff(times)[0]
+time_frequency = int(1 / time_interval)
+
+
 times = np.linspace(0, 1, 10_000)
 
 for time in times:
-    print(ball_two.pos)
+    vp.rate(0.25*time_frequency)
     ball_two.pos += ball_two_velocity * time
